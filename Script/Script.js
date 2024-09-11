1c29cce1c4b76e242e60d63dce5ad1532756b356
Luisda94 <134888722+Luisda94@users.noreply.github.com>
2024-09-11 16:23:21 -0300

    Update 1.2 Script.js
    Menu desplegable

--- Script/Script.js
+++ Script/Script.js
@@ -5,7 +5,22 @@ document.addEventListener('DOMContentLoaded', function() {
         const userChoice = confirm("El archivo PDF se abrirá en una nueva pestaña. ¿Desea continuar?");
         if (!userChoice) {
             event.preventDefault();
-        }
+        
+document.addEventListener('DOMContentLoaded', function() {
+    const collapsibles = document.querySelectorAll('.collapsible-button');
+    collapsibles.forEach(button => {
+        button.addEventListener('click', function() {
+            this.classList.toggle('active');
+            const content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
     });
 });
+</script>
+
+
 </script>
